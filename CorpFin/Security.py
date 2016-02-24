6e6005458f01c5e57976546de425d04d372501cb
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-02-24 13:05:14 -0600

    add Security class

---
+++ CorpFin/Security.py
@@ -0,0 +1,14 @@
+
+
+class Security:
+    def __init__(self, name='', par=0., val=0.):
+        self.name = name
+        self.par = par
+        self.val = val
+
+    def __repr__(self):
+        if self.name:
+            s = ' "%s"' % self.name
+        else:
+            s = ''
+        return 'Security' + s + ': Par = %.3g, Val = %.3g' % (self.par, self.val)

b4a7ab2d49095fcc42c9958b14809762505c79ef
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-02-24 14:20:52 -0600

    update Portfolio class

--- CorpFin/Portfolio.py
+++ CorpFin/Portfolio.py
@@ -1,27 +1,36 @@
-from numpy import nan
 from pandas import DataFrame
 from .Security import Security
 
 
-def val(x):
-    if isinstance(x, Security):
-        return x.val
+def val(asset):
+    if isinstance(asset, Security):
+        return asset.val
     else:
-        return x.val()
+        return asset.val()
 
 
 class Portfolio:
-    def __init__(self, *n_assets):
-        self.c = [(na[0], na[1]) if isinstance(na, (list, tuple)) else (1, na)
-                  for na in n_assets]
+    def __init__(self, name, *n_assets):
+        self.name = name
+        self.c = [(x[0], x[1]) if isinstance(x, (list, tuple)) else (1, x)
+                  for x in n_assets]
 
     def val(self):
         return reduce(lambda x, y: x + y, map(lambda x: x[0] * val(x[1]), self.c))
 
     def __repr__(self):
         df = DataFrame(columns=['n', 'asset', 'val'])
-        df.loc['total'] = [nan, nan, self.val()]
+        df.loc[''] = ['', 'TOTAL', self.val()]
         for i in range(len(self.c)):
             n, asset = self.c[i]
-            df.loc[i] = [n, asset, n * val(asset)]
+            v = val(asset)
+            if isinstance(asset, Portfolio):
+                if asset.name:
+                    s = ' "%s"' % asset.name
+                else:
+                    s = ''
+                asset = 'Portfolio' + s + ': Val = %.3g' % v
+            else:
+                asset = repr(asset)
+            df.loc[i] = [n, asset, n * v]
         return repr(df)

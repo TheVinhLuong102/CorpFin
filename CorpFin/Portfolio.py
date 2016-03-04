f5c8bc145d1aba49b818f558c13f7cefb9ae5bd7
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-03-04 02:44:46 -0600

    add label representation of Portfolio

--- CorpFin/Portfolio.py
+++ CorpFin/Portfolio.py
@@ -14,9 +14,13 @@ class Portfolio:
     def __init__(self, label, *n_assets):
         self.label = label
         n_asset = namedlist('N_Asset', ['n', 'asset'])
-        self.c = [n_asset(n=x[0], asset=x[1]) if isinstance(x, (list, tuple))
-                  else n_asset(n=1, asset=x)
-                  for x in n_assets]
+        self.c = \
+            [n_asset(n=x[0], asset=x[1]) if isinstance(x, (list, tuple))
+             else n_asset(n=1, asset=x)
+             for x in n_assets]
+        self.label_repr = \
+            [n_asset(n=x.n, asset=x.asset.label)
+             for x in self.c]
 
     def val(self, **kwargs):
         return reduce(lambda x, y: x + y, map(lambda x: x.n * val(x.asset, **kwargs), self.c))

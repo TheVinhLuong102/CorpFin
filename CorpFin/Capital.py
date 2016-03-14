5bfc9ce1c43a3a091bbc1f3c35d8f7602c269406
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-03-14 03:39:53 -0500

    fix bug in Capital module

--- CorpFin/Capital.py
+++ CorpFin/Capital.py
@@ -410,7 +410,7 @@ class CapitalStructure:
         security_vals = val_results['security_vals']
         if ownerships:
             df = DataFrame(columns=['Owner', 'Security', 'Val', 'Share'])
-            common_share_val = self[self.common_share_label].n * security_vals[self.common_share_label]
+            common_share_val = capital_structure[self.common_share_label].n * security_vals[self.common_share_label]
             i = 0
             for owner, holdings in capital_structure.ownerships.items():
                 for security_label, quantity in holdings.items():
